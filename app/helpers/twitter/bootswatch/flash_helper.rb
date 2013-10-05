c60c15b0c973edc791218552451de35ef7d40870
Scott V. Rosenthal <srosenthal@clickhere.com>
2013-10-05 12:40:41 -0500

    fix for alert dismissable

--- app/helpers/twitter/bootswatch/flash_helper.rb
+++ app/helpers/twitter/bootswatch/flash_helper.rb
@@ -7,7 +7,11 @@ module Twitter
         flash_messages = ''
         flash.each do |type, message|
           alert_type_class = bootswatch_alert_types(type)
+          if alert_dismissable
           alert_div = content_tag(:div, raw('<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>') + message, :class => "alert #{alert_type_class}#{alert_dismissable_class}")
+          else
+            alert_div = content_tag(:div, raw(message), :class => "alert #{alert_type_class}")
+          end
           flash_messages << alert_div if message
         end
         raw(flash_messages)
@@ -27,9 +31,15 @@ module Twitter
       def bootswatch_flash_container(type, alert_dismissable=true, &message)
         alert_dismissable_class = alert_dismissable ? ' alert-dismissable' : ''
         alert_type_class = bootswatch_alert_types(type)
-        message_text = capture(&message)
-        output = content_tag(:div, :class => "alert #{alert_type_class}#{alert_dismissable_class}") do
-          '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>'.html_safe.safe_concat(message_text)
+        message_html = capture(&message)
+        if alert_dismissable
+          output = content_tag(:div, :class => "alert #{alert_type_class}#{alert_dismissable_class}") do
+            '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>'.html_safe.safe_concat(message_html)
+          end
+        else
+          output = content_tag(:div, :class => "alert #{alert_type_class}") do
+            message_html.html_safe
+          end
         end
 
         raw(output)
--- lib/twitter/bootswatch/rails/helpers/version.rb
+++ lib/twitter/bootswatch/rails/helpers/version.rb
@@ -2,7 +2,7 @@ module Twitter
   module Bootswatch
     module Rails
       module Helpers
-        VERSION = '3.0.0.0'
+        VERSION = '3.0.0.1'
       end
     end
   end
