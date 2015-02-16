67a68db225af7fb03265a6ef3bd69acec3cba28e
scottvrosenthal <sr7575@gmail.com>
2015-02-16 14:05:29 -0600

    resolve more merge conflicts

--- lib/twitter/bootswatch/rails/helpers/version.rb
+++ lib/twitter/bootswatch/rails/helpers/version.rb
@@ -2,11 +2,7 @@ module Twitter
   module Bootswatch
     module Rails
       module Helpers
-<<<<<<< HEAD
         VERSION = '3.3.2.0'
-=======
-        VERSION = '3.2.0.0'
->>>>>>> 4da4344c75742369ebdd98810a3678d71dbfaaf1
       end
     end
   end
